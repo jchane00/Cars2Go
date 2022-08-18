060afb5f4b9793f4802e823ab155559728fa7fad
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-08-18 13:40:44 -0500

    resolving merge conflicts

--- server.js
+++ server.js
@@ -4,8 +4,6 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-const session = require('express-session');
-const { extname } = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
@@ -26,11 +24,6 @@ app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(session({
-  secret: "Super secret",
-  resave: false,
-  saveUninitialized: true
-}))
 // turn on routes
 app.use(routes);
 
