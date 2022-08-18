859a5ca1c3f2568b5971869ee6e2634b1249d2ce
ashwini lalithaHuchahanumegowda <ashwini.lh12@gmail>
2022-08-18 13:57:23 -0500

    updating home routes and addcars button

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,12 +1,17 @@
 
 const router = require('express').Router();
+const { Cars } = require('../models');
 
 router.get('/login', async (req,res) => {
     res.render('login')
   });
 
 router.get('/', async (req,res) => {
-    res.render('homepage')
+  const carsData = await Cars.findAll();
+    const cars = carsData.map((car) => car.get({ plain: true }));
+
+    res.render('cars', { cars, loggedIn: !!req.session.user_id });
+    //res.render('homepage')
   });
   module.exports = router;
 
